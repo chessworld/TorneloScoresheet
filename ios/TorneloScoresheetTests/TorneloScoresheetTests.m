8504125f971fed422ca971e375c4a57147454ab1
Harrison Marshall <harry130899@hotmail.com>
2022-03-31 14:54:15 +1100

    Renamed the project to `TorneloScoresheet`
    Used `react-native-rename` to rename the app :^)

--- ios/TorneloScoresheetTests/TorneloScoresheetTests.m
+++ ios/TorneloScoresheetTests/TorneloScoresheetTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChessScoresheetTests : XCTestCase
+@interface TorneloScoresheetTests : XCTestCase
 
 @end
 
-@implementation ChessScoresheetTests
+@implementation TorneloScoresheetTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
