c89f6f43e7b0515afdbcc5cf8bccef36b5a0f1c3
Harrison Marshall <hmar0007@student.monash.edu>
2022-04-02 11:51:55 +1100

    App: Render `ArbiterSetup` when the state is `ArbiterSetupMode`
    This is the only mode we support so far. In future we will support the
    other modes.

---
+++ src/pages/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {useAppState} from '../context/AppStateContext';
+import {AppStateVariant} from '../types/AppState';
+import ArbiterSetup from './ArbiterSetup';
+
+const Main: React.FC = () => {
+  const [appState] = useAppState();
+  return (
+    <>
+      {appState.mode === AppStateVariant.ArbiterSetup ? (
+        <ArbiterSetup />
+      ) : (
+        <Text>Unsupported app mode</Text>
+      )}
+    </>
+  );
+};
+
+export default Main;
