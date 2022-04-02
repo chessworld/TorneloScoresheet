cd096d47468cd05439b0c655d6542bb61365e09c
Harrison Marshall <hmar0007@student.monash.edu>
2022-04-02 12:23:25 +1100

    App: Updated `prettierrc` to enforce bracket spacing
    This is on by default, so let's just remove the line that turns it off.

--- src/pages/ArbiterSetup.tsx
+++ src/pages/ArbiterSetup.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-import {useAppState} from '../context/AppStateContext';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useAppState } from '../context/AppStateContext';
 
 const ArbiterSetup: React.FC = () => {
-  const [, {enterTablePairingMode}] = useAppState();
+  const [, { enterTablePairingMode }] = useAppState();
 
   const handleNextClick = () => {
     // FIXME: Use the URL entered into the `TextInput` below
