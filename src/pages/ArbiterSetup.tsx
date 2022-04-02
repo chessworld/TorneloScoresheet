6b27931014f65234e67c2ca4c2ccd005fe1ad529
Harrison Marshall <hmar0007@student.monash.edu>
2022-04-02 11:45:02 +1100

    App: Added the `ArbiterSetup` page
    This page allows arbiters to enter the URL of a live game PGN link.
    
    This is very incomplete, it is nothing more than a proof of concept that
    we can access and modify the app state. In future, we will have to
    implement the UI design as per figma, and track the state of the text
    input

---
+++ src/pages/ArbiterSetup.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useAppState} from '../context/AppStateContext';
+
+const ArbiterSetup: React.FC = () => {
+  const [, {enterTablePairingMode}] = useAppState();
+
+  const handleNextClick = () => {
+    // FIXME: Use the URL entered into the `TextInput` below
+    enterTablePairingMode('FAKE URL');
+  };
+
+  return (
+    <View style={styles.arbiterSetup}>
+      <View>
+        <Text>Enter Game Link:</Text>
+        <TextInput placeholder="Game Link" />
+      </View>
+      <Button title="Next" onPress={handleNextClick} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  arbiterSetup: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
+
+export default ArbiterSetup;
