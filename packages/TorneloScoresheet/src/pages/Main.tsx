ee43ac8f71a689b7a0d8e821c70875adb2eaad76
mham0004 <mham0004@student.monash.edu>
2022-04-15 16:22:42 +1000

    remove testing code in main activity and stop running pipeline on this branch

--- packages/TorneloScoresheet/src/pages/Main.tsx
+++ packages/TorneloScoresheet/src/pages/Main.tsx
@@ -3,16 +3,9 @@ import { Text } from 'react-native';
 import { useAppModeState } from '../context/AppModeStateContext';
 import { AppMode } from '../types/AppModeState';
 import ArbiterSetup from './ArbiterSetup';
-import { useChessGameStateContext } from '../context/ChessGaneStateContext';
 
 const Main: React.FC = () => {
   const [{ mode: appMode }] = useAppModeState();
-  const [{ playMove, generatePgn }] = useChessGameStateContext();
-
-  for(let i=0; i<10;i++){
-    console.log(playMove());
-  }
-  console.log(generatePgn());
   
   return (
     <>
