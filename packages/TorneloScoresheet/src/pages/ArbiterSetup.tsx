e31fe417a19b1391d520865fb48e0e7cf71e0c86
Harrison Marshall <hmar0007@student.monash.edu>
2022-04-16 16:27:49 +1000

    ArbiterSetup screen: Added state for the url input field
    Also added a check to see if the URL entered is valid, in future we will
    render a relevant error if the url is invalid.
    
    This involved adding the URL polyfill added in a previous commit to
    imports in `index.js`

--- packages/TorneloScoresheet/src/pages/ArbiterSetup.tsx
+++ packages/TorneloScoresheet/src/pages/ArbiterSetup.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useAppModeState } from '../context/AppModeStateContext';
+import { validUrl } from '../util/url';
 
 const ArbiterSetup: React.FC = () => {
   const [, { enterTablePairingMode }] = useAppModeState();
+  const [url, setUrl] = useState('');
 
-  const handleNextClick = () => {
-    // FIXME: Use the URL entered into the `TextInput` below
-    enterTablePairingMode('FAKE URL');
+  const handleNextClick = async () => {
+    if (!validUrl(url)) {
+      // TODO: Render error on text input
+      console.log('Invalid url', url);
+      return;
+    }
+
+    const result = await enterTablePairingMode(url);
+    // TODO: Show the user an error if this is an error
+    result;
   };
 
   return (
     <View style={styles.arbiterSetup}>
       <View>
         <Text>Enter Game Link:</Text>
-        <TextInput placeholder="Game Link" />
+        <TextInput onChangeText={setUrl} value={url} placeholder="Game Link" />
       </View>
       <Button title="Next" onPress={handleNextClick} />
     </View>
