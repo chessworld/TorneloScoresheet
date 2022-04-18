020093a212d5d62e0f2de0fd6290ea76291f1c81
mham0004 <mham0004@student.monash.edu>
2022-04-18 15:34:50 +1000

    extract game info from pgn

--- packages/TorneloScoresheet/src/arbiterSetupMode.ts
+++ packages/TorneloScoresheet/src/arbiterSetupMode.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import { parseGameInfo } from './chessEngine';
 import { AppMode, AppModeState } from './types/AppModeState';
+import { GameInfo } from './types/chessGameInfo';
 import { Result } from './types/Result';
 
 /**
@@ -12,25 +14,60 @@ export const makeEnterTablePairingMode =
   (
     setAppMode: React.Dispatch<React.SetStateAction<AppModeState>>,
   ): ((liveLinkUrl: string) => Promise<Result>) =>
-  async (liveLinkUrl: string) => {
-    const result = await axios.get(liveLinkUrl, { validateStatus: () => true });
+    async (liveLinkUrl: string) => {
+      // fetch pgn from api
+      const result = await axios.get(liveLinkUrl, { validateStatus: () => true });
 
-    if (result.status !== 200) {
-      // TODO: Return a proper error
+      if (result.status !== 200) {
+        // TODO: Return a proper error
+        return '';
+      }
+      if (typeof result.data !== 'string') {
+        // TODO: Return a proper error
+        return '';
+      }
+      const roundPgn = result.data;
+
+      // split into multiple pgn per pairing
+      const paringPgns = splitRoundIntoMultiplePgn(roundPgn)
+
+      let pairings: GameInfo[] = []
+
+      // extract gameinfo from each pgn 
+      if (paringPgns != undefined) {
+
+        paringPgns.forEach(pgn => {
+          // parse pgn as gameinfo
+          let gameInfo = parseGameInfo(pgn)
+
+          // add to array if no error
+          if (gameInfo != undefined) {
+            pairings.push(gameInfo)
+          }
+        })
+      }
+
+      // TODO
+      // 3. Set new game state
+      setAppMode({ mode: AppMode.TablePairing, games: 0, pairings: pairings });
       return '';
+    };
+
+
+const splitRoundIntoMultiplePgn = (roundPgns: string): string[] | undefined => {
+  try {
+
+    let rounds = roundPgns.split(/\n{3}/g)
+
+    // if more than 1 pairing, last element will be empty string -> remove
+    if (rounds.length > 2) {
+      rounds.splice(rounds.length - 2)
     }
-    if (typeof result.data !== 'string') {
-      // TODO: Return a proper error
-      return '';
-    }
+    return rounds
 
-    const pgn = result.data;
 
-    console.log(pgn);
-
-    // TODO
-    // 2. Parse PGN
-    // 3. Set new game state
-    setAppMode({ mode: AppMode.TablePairing, games: 0 });
-    return '';
-  };
+  } catch {
+    // error occured -> return undefined
+    return undefined
+  }
+}
