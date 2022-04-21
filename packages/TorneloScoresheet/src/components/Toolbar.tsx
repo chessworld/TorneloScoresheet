3a54ef87a2b1006b5e39772d4665348412ebb4db
gestureleft <harry130899@hotmail.com>
2022-04-21 16:15:33 +1000

    Added a help sheet and button to the toolbar

--- packages/TorneloScoresheet/src/components/Toolbar.tsx
+++ packages/TorneloScoresheet/src/components/Toolbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { useAppModeState } from '../context/AppModeStateContext';
 import {
@@ -9,6 +9,15 @@ import {
 } from '../style/colour';
 import { primary as primaryFont } from '../style/font';
 import { AppMode } from '../types/AppModeState';
+import IconButton from './IconButton';
+import Sheet from './Sheet';
+
+/**
+ * The App's toolbar.
+ *
+ * Depending on what mode the app is in, the way the toolbar looks will be different,
+ * as well as what actions and buttons are available
+ */
 
 const BLACK_LOGO_IMAGE = require('../../assets/images/icon-logo-black-500.png');
 const WHITE_LOGO_IMAGE = require('../../assets/images/icon-logo-white-500.png');
@@ -26,21 +35,40 @@ const backgroundColorStyle = (backgroundColor: string) => ({
 
 const Toolbar: React.FC = () => {
   const [appModeState] = useAppModeState();
+  const [showSheet, setShowSheet] = useState(false);
+  const handleHelpPress = () => {
+    setShowSheet(a => !a);
+  };
   const currentColour = colourForMode[appModeState.mode];
   const currentTextColour = textColour(currentColour);
   return (
     <>
       <StatusBar barStyle={statusBarStyleForColor(currentColour)} />
+      <Sheet
+        title="Help"
+        dismiss={() => setShowSheet(false)}
+        visible={showSheet}
+        content="Put help here..."
+      />
       <View style={[styles.container, backgroundColorStyle(currentColour)]}>
-        <Image
-          style={styles.logoImage}
-          source={
-            currentTextColour === 'black' ? BLACK_LOGO_IMAGE : WHITE_LOGO_IMAGE
-          }
+        <View style={styles.logo}>
+          <Image
+            style={styles.logoImage}
+            source={
+              currentTextColour === 'black'
+                ? BLACK_LOGO_IMAGE
+                : WHITE_LOGO_IMAGE
+            }
+          />
+          <Text style={[styles.logoText, { color: currentTextColour }]}>
+            Tornelo
+          </Text>
+        </View>
+        <IconButton
+          icon="help"
+          onPress={handleHelpPress}
+          colour={currentTextColour}
         />
-        <Text style={[styles.logoText, { color: currentTextColour }]}>
-          Tornelo
-        </Text>
       </View>
     </>
   );
@@ -55,6 +83,11 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    display: 'flex',
+    flexDirection: 'row',
   },
   logoImage: {
     width: 40,
