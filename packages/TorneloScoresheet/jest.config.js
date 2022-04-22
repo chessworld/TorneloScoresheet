fd7ec2af10874504c952aaa7153f1d1539793cd4
gestureleft <harry130899@hotmail.com>
2022-04-22 13:09:48 +1000

    Moved Jest config to it's own file
    Also added "jest-native" testing library dependency
    
    These changes are to allow us to import images in components we render
    for testing. Without these changes Jest complains about trying to import
    anything that isn't Javascript

---
+++ packages/TorneloScoresheet/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
+  ],
+};
