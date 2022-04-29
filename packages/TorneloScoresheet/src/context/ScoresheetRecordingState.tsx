7c33965a74a7b39aed9fc4f5d381c609f6289488
Malo Hamon <malo.hamon@why-innovation.com>
2022-04-29 15:58:51 +1000

    remove scoresheet state (belogs in another branch)

--- packages/TorneloScoresheet/src/context/ScoresheetRecordingState.tsx
+++
@@ -1,4 +0,0 @@
-import { PlayerScoresheetRecordingMode } from '../types/AppModeState';
-import { useAppModeState } from './AppModeStateContext';
-
-type ScoresheetRecordingHookStateType = [PlayerScoresheetRecordingMode, {}];
