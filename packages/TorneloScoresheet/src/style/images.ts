318d42c12ffce33d4415d8492098045a0dafab1d
gestureleft <harry130899@hotmail.com>
2022-05-01 15:52:27 +1000

    Added svg imports and re-exports for piece images

--- packages/TorneloScoresheet/src/style/images.ts
+++ packages/TorneloScoresheet/src/style/images.ts
@@ -1,2 +1,16 @@
 export const BLACK_LOGO_IMAGE = require('../../assets/images/icon-logo-black-500.png');
 export const WHITE_LOGO_IMAGE = require('../../assets/images/icon-logo-white-500.png');
+
+import BISHOP_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_bishop_black.svg';
+import PAWN_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_pawn_black.svg';
+import ROOK_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_rook_black.svg';
+import KNIGHT_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_knight_black.svg';
+import QUEEN_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_queen_black.svg';
+import KING_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_king_black.svg';
+
+export const BISHOP = BISHOP_ASSET;
+export const PAWN = PAWN_ASSET;
+export const ROOK = ROOK_ASSET;
+export const KNIGHT = KNIGHT_ASSET;
+export const QUEEN = QUEEN_ASSET;
+export const KING = KING_ASSET;
