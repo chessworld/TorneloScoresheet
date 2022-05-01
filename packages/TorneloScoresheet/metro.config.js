b77d1a95d4effbf790b513db85876387e67b1af6
gestureleft <harry130899@hotmail.com>
2022-05-01 15:50:40 +1000

    Installed "react-native-svg" and "react-native-svg-transformer"
    We will be using these to render chess pieces.
    
    Also added config for packaging svgs, and added a ts declaration file to
    give svg imports a type

--- packages/TorneloScoresheet/metro.config.js
+++ packages/TorneloScoresheet/metro.config.js
@@ -5,19 +5,28 @@
  * @format
  */
 const packagePath = '../chess.ts';
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = {
-  resolver: {
-    nodeModulesPaths: [packagePath],
-    // rest of metro resolver options...
-  },
-  watchFolders: [packagePath],
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      nodeModulesPaths: [packagePath],
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      // rest of metro resolver options...
+    },
+    watchFolders: [packagePath],
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+  };
+})();
