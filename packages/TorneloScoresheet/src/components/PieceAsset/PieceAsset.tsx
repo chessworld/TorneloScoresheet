612260685a8c3700643f6c2d7a5c83a8ee184bac
gestureleft <harry130899@hotmail.com>
2022-05-04 22:19:32 +1000

    `PieceAsset`: Added support for different colours and styling
    This is in preparation for rendering the King on the EnterPGN page in
    the Secondary colour

--- packages/TorneloScoresheet/src/components/PieceAsset/PieceAsset.tsx
+++ packages/TorneloScoresheet/src/components/PieceAsset/PieceAsset.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+import { Color } from 'react-native-svg';
 import { colours } from '../../style/colour';
 import { BISHOP, KNIGHT, QUEEN, ROOK, KING, PAWN } from '../../style/images';
 import { PlayerColour } from '../../types/ChessGameInfo';
@@ -7,24 +9,83 @@ import { PieceType, Piece } from '../../types/ChessMove';
 export type PieceAssetProps = {
   piece: Piece;
   size: number;
+  colour?: Color | undefined;
+  style?: StyleProp<ViewStyle>;
 };
 
-const PieceAsset: React.FC<PieceAssetProps> = ({ piece, size }) => {
+const PieceAsset: React.FC<PieceAssetProps> = ({
+  piece,
+  size,
+  colour,
+  style,
+}) => {
   const fill =
-    piece.player === PlayerColour.Black ? colours.black : colours.white;
+    colour ??
+    (piece.player === PlayerColour.Black ? colours.black : colours.white);
+  const pieceStroke =
+    colour ??
+    (piece.player === PlayerColour.White ? colours.black : colours.white);
   switch (piece.type) {
     case PieceType.Bishop:
-      return <BISHOP height={size} width={size} fill={fill} />;
+      return (
+        <BISHOP
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     case PieceType.Rook:
-      return <ROOK height={size} width={size} fill={fill} />;
+      return (
+        <ROOK
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     case PieceType.King:
-      return <KING height={size} width={size} fill={fill} />;
+      return (
+        <KING
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     case PieceType.Queen:
-      return <QUEEN height={size} width={size} fill={fill} />;
+      return (
+        <QUEEN
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     case PieceType.Knight:
-      return <KNIGHT height={size} width={size} fill={fill} />;
+      return (
+        <KNIGHT
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     case PieceType.Pawn:
-      return <PAWN height={size} width={size} fill={fill} />;
+      return (
+        <PAWN
+          style={style}
+          height={size}
+          width={size}
+          fill={fill}
+          stroke={pieceStroke}
+        />
+      );
     default:
       return <>Unknown Piece</>;
   }
