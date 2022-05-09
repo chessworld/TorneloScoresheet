f070f079eca043059327e1b29aef8e1a04a8c981
gestureleft <harry130899@hotmail.com>
2022-05-09 19:49:26 +1000

    Added `Draggable` component
    This is a component that can be dragged across the screen, complete with
    animation and hit testing for drop targets

---
+++ packages/TorneloScoresheet/src/components/DragAndDrop/Draggable/Draggable.tsx
@@ -0,0 +1,67 @@
+import React, { useRef } from 'react';
+import { StyleProp, View, ViewStyle } from 'react-native';
+import {
+  PanGestureHandlerGestureEvent,
+  PanGestureHandler,
+  PanGestureHandlerEventPayload,
+} from 'react-native-gesture-handler';
+import Animated, {
+  withSpring,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  runOnJS,
+} from 'react-native-reanimated';
+import { useHitTest } from '../DragAndDropContext/DragAndDropContext';
+
+type DraggableProps = {
+  data: unknown;
+  style?: StyleProp<ViewStyle>;
+};
+
+const Draggable: React.FC<DraggableProps> = ({ children, data, style }) => {
+  const startingPosition = 0;
+  const x = useSharedValue(startingPosition);
+  const y = useSharedValue(startingPosition);
+  const viewRef = useRef<View>(null);
+
+  const hitTest = useHitTest(viewRef, data, () => {
+    x.value = withSpring(0, { damping: 200, stiffness: 900 });
+    y.value = withSpring(0, { damping: 200, stiffness: 900 });
+  });
+
+  const eventHandler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    PanGestureHandlerEventPayload & { startX: number; startY: number }
+  >({
+    onStart: (_event, ctx) => {
+      ctx.startX = x.value;
+      ctx.startY = y.value;
+    },
+    onActive: (event, ctx) => {
+      x.value = ctx.startX + event.translationX;
+      y.value = ctx.startY + event.translationY;
+    },
+    onEnd: (_event, _ctx) => {
+      runOnJS(hitTest)();
+    },
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: x.value }, { translateY: y.value }],
+    };
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={eventHandler}>
+      <Animated.View style={animatedStyle}>
+        <View style={style} ref={viewRef}>
+          {children}
+        </View>
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+export default Draggable;
