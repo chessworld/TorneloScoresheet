67ef081baa70977f9febe3e48d465d7feba40ad1
Malo Hamon <hamon.malo@gmail.com>
2022-05-22 14:26:57 +1000

    Added icon Image assets
    New buttonIcons folder in assets
    Linked all the svgs as exported constants

--- packages/TorneloScoresheet/src/style/images.ts
+++ packages/TorneloScoresheet/src/style/images.ts
@@ -1,6 +1,7 @@
 export const BLACK_LOGO_IMAGE = require('../../assets/images/icon-logo-black-500.png');
 export const WHITE_LOGO_IMAGE = require('../../assets/images/icon-logo-white-500.png');
 
+// chess pieces
 import BISHOP_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_bishop_black.svg';
 import PAWN_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_pawn_black.svg';
 import ROOK_ASSET from '../../assets/images/chessPieces/Tornelo Pieces_rook_black.svg';
@@ -14,3 +15,20 @@ export const ROOK = ROOK_ASSET;
 export const KNIGHT = KNIGHT_ASSET;
 export const QUEEN = QUEEN_ASSET;
 export const KING = KING_ASSET;
+
+// Button Icons
+import ICON_FLIP_ASSET from '../../assets/images/buttonIcons/icon_flip.svg';
+import ICON_SKIP_ASSET from '../../assets/images/buttonIcons/icon_skip.svg';
+import ICON_HALF_ASSET from '../../assets/images/buttonIcons/icon_half.svg';
+import ICON_HASHTAG_ASSET from '../../assets/images/buttonIcons/icon_hashtag.svg';
+import ICON_REPEAT_ASSET from '../../assets/images/buttonIcons/icon_repeat.svg';
+import ICON_UNDO_ASSET from '../../assets/images/buttonIcons/icon_undo.svg';
+import ICON_CLOCK_ASSET from '../../assets/images/buttonIcons/icon_clock.svg';
+
+export const ICON_FLIP = ICON_FLIP_ASSET;
+export const ICON_SKIP = ICON_SKIP_ASSET;
+export const ICON_HALF = ICON_HALF_ASSET;
+export const ICON_HASTAG = ICON_HASHTAG_ASSET;
+export const ICON_REPEAT = ICON_REPEAT_ASSET;
+export const ICON_UNDO = ICON_UNDO_ASSET;
+export const ICON_CLOCK = ICON_CLOCK_ASSET;
