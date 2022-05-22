1e33c8abff935cfe24d74ebc1ebfb114d83f8a65
Malo Hamon <hamon.malo@gmail.com>
2022-05-22 14:27:43 +1000

    Added the action bar to the graphical recoding page
    The buttons are as they are on figma
    This is easy to change
    Only the flip button currently works

---
+++ packages/TorneloScoresheet/src/pages/GraphicalRecording/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    marginHorizontal: 10,
+  },
+  boardButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+});
