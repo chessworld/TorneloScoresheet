0c7600ad2cca7722500a3e590a943ceafcb588b1
Malo Hamon <hamon.malo@gmail.com>
2022-05-26 17:33:11 +1000

    Created `TextIconButton` Component
    Text Icon Button is a button with either text, an icon or both

---
+++ packages/TorneloScoresheet/src/components/TextIconButton/TextIconButton.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  StyleProp,
+  TextStyle,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
+import { SvgProps } from 'react-native-svg';
+import { colours } from '../../style/colour';
+import PrimaryText from '../PrimaryText/PrimaryText';
+import RoundedView from '../RoundedView/RoundedView';
+import { styles } from './style';
+
+type TextIconButtonParms = {
+  Icon?: React.FC<SvgProps>;
+  text?: string;
+  buttonHeight?: number;
+  onPress: () => void;
+  buttonTextStyle?: StyleProp<TextStyle>;
+} & TouchableOpacityProps;
+
+const TextIconButton: React.FC<TextIconButtonParms> = ({
+  Icon,
+  text,
+  buttonHeight,
+  onPress,
+  buttonTextStyle,
+  ...touchableOpacityProps
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.button}
+      {...touchableOpacityProps}>
+      <RoundedView
+        style={[
+          styles.buttonContainer,
+          { height: buttonHeight && buttonHeight + 10 },
+        ]}>
+        {Icon && (
+          <Icon
+            fill={colours.white}
+            stroke={colours.white}
+            height={buttonHeight}
+            width={buttonHeight}
+          />
+        )}
+
+        {text && (
+          <PrimaryText style={[styles.buttonText, buttonTextStyle]}>
+            {text}
+          </PrimaryText>
+        )}
+      </RoundedView>
+    </TouchableOpacity>
+  );
+};
+
+export default TextIconButton;
