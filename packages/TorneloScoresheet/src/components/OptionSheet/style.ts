9c29b4daf2fa7971921f7062d844c5ecf7f86d95
Malo Hamon <hamon.malo@gmail.com>
2022-05-26 15:23:56 +1000

    Created `OptionPopup` Component
    This is a popup that prompts the user to click on a button
    
    There can be multiple buttons, each with an onPress function
    
    The buttons can have text, and icon or both text and an icon

---
+++ packages/TorneloScoresheet/src/components/OptionSheet/style.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { colours } from '../../style/colour';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+  },
+  buttonText: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: colours.white,
+  },
+  button: {
+    alignItems: 'center',
+    width: 200,
+    backgroundColor: colours.primary,
+    marginVertical: 10,
+    borderRadius: 16,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  buttonArea: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  messageText: {
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+});
