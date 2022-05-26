b8894e4033121a5e5d44726735bb0e31dfa4f27d
Malo Hamon <hamon.malo@gmail.com>
2022-05-26 15:23:56 +1000

    Created `OptionPopup` Component
    This is a popup that prompts the user to click on a button
    
    There can be multiple buttons, each with an onPress function
    
    The buttons can have text, and icon or both text and an icon

---
+++ packages/TorneloScoresheet/src/components/OptionSheet/OptionSheet.tsx
@@ -0,0 +1,56 @@
+import { View } from 'react-native';
+import { SvgProps } from 'react-native-svg';
+import Sheet from '../Sheet/Sheet';
+import { styles } from './style';
+import { colours } from '../../style/colour';
+import PrimaryText, { FontWeight } from '../PrimaryText/PrimaryText';
+import React from 'react';
+import TextIconButton from '../TextIconButton/TextIconButton';
+
+type Option = {
+  icon?: React.FC<SvgProps>;
+  text?: string;
+  onPress: () => void;
+};
+type OptionSheetProps = {
+  message: string;
+  options: Option[];
+  visible: boolean;
+  onCancel: () => void;
+};
+
+const OptionSheet: React.FC<OptionSheetProps> = ({
+  message,
+  options,
+  visible,
+  onCancel,
+}) => {
+  return (
+    <Sheet dismiss={onCancel} visible={visible}>
+      <View style={styles.mainContainer}>
+        <PrimaryText
+          style={styles.messageText}
+          weight={FontWeight.Bold}
+          size={30}
+          colour={colours.darkenedElements}>
+          {message}
+        </PrimaryText>
+        <View style={styles.buttonArea}>
+          {options.map((option, i) => (
+            <TextIconButton
+              Icon={option.icon}
+              text={option.text}
+              buttonHeight={60}
+              onPress={option.onPress}
+              key={'button-' + i.toString()}
+              buttonTextStyle={styles.buttonText}
+              style={styles.button}
+            />
+          ))}
+        </View>
+      </View>
+    </Sheet>
+  );
+};
+
+export default OptionSheet;
