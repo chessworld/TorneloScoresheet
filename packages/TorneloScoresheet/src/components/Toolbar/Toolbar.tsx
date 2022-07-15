f776c3be4636a5e7beceab556a165f00660e020b
gwenllian1 <llio.vernon@gmail.com>
2022-07-15 14:45:09 +1000

    created arbiter mode button that is visible in player mode

--- packages/TorneloScoresheet/src/components/Toolbar/Toolbar.tsx
+++ packages/TorneloScoresheet/src/components/Toolbar/Toolbar.tsx
@@ -7,12 +7,17 @@ import {
   statusBarStyleForColor,
   textColour,
 } from '../../style/colour';
-import { BLACK_LOGO_IMAGE, WHITE_LOGO_IMAGE } from '../../style/images';
+import {
+  BLACK_LOGO_IMAGE,
+  ICON_ARBITER_MODE,
+  WHITE_LOGO_IMAGE,
+} from '../../style/images';
 import { AppMode } from '../../types/AppModeState';
 import IconButton from '../IconButton/IconButton';
 import PrimaryText, { FontWeight } from '../PrimaryText/PrimaryText';
 import Sheet from '../Sheet/Sheet';
 import { styles } from './style';
+import ToolbarButton from './ToolbarButton';
 
 /**
  * The App's toolbar.
@@ -30,6 +35,14 @@ const colourForMode: Record<AppMode, ColourType> = {
   [AppMode.TablePairing]: colours.primary,
 };
 
+const arbiterModeDisplay: Record<AppMode, 'none' | undefined> = {
+  [AppMode.EnterPgn]: 'none',
+  [AppMode.PariringSelection]: 'none',
+  [AppMode.TablePairing]: undefined,
+  [AppMode.GraphicalRecording]: undefined,
+  [AppMode.ResultDisplay]: undefined,
+};
+
 const backgroundColorStyle = (backgroundColor: string) => ({
   backgroundColor,
 });
@@ -37,10 +50,15 @@ const backgroundColorStyle = (backgroundColor: string) => ({
 const Toolbar: React.FC = () => {
   const appModeState = useAppModeState();
   const [showSheet, setShowSheet] = useState(false);
+  const [showArbiterSheet, setShowArbiterSheet] = useState(false);
   const handleHelpPress = () => {
     setShowSheet(a => !a);
   };
+  const handleArbiterPress = () => {
+    setShowArbiterSheet(a => !a);
+  };
   const currentColour = colourForMode[appModeState.mode];
+  const arbiterModeVisibility = arbiterModeDisplay[appModeState.mode];
   const currentTextColour = textColour(currentColour);
   return (
     <>
@@ -51,7 +69,16 @@ const Toolbar: React.FC = () => {
         visible={showSheet}>
         <PrimaryText>Put help here!</PrimaryText>
       </Sheet>
+      <Sheet
+        title="Enter Pin"
+        dismiss={() => setShowArbiterSheet(false)}
+        visible={showArbiterSheet}></Sheet>
       <View style={[styles.container, backgroundColorStyle(currentColour)]}>
+        <ToolbarButton
+          Icon={ICON_ARBITER_MODE}
+          onPress={handleArbiterPress}
+          colour={currentColour}
+          display={arbiterModeVisibility}></ToolbarButton>
         <View style={styles.logo}>
           <Image
             style={styles.logoImage}
