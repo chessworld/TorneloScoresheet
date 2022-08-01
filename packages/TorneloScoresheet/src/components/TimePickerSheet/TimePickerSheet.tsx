63b720cfa39d1b7d0a5242397f8c15f30f78cf57
Malo Hamon <hamon.malo@gmail.com>
2022-08-01 12:23:47 +1000

    Created `TimePickerSheet` component
    This is a sheet that will allow the user to select a time for a move
    
    The sheet has 2 buttons:
    - confirm sets the game time
    - remove will set the game time to undefined

---
+++ packages/TorneloScoresheet/src/components/TimePickerSheet/TimePickerSheet.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import { GameTime } from '../../types/ChessMove';
+import PrimaryButton from '../PrimaryButton/PrimaryButton';
+import Sheet from '../Sheet/Sheet';
+import { styles } from './style';
+
+export type TimePickerSheetProps = {
+  gameTime: GameTime;
+  visible: boolean;
+  dismiss: () => void;
+  setGameTime: (gameTime: GameTime | undefined) => void;
+};
+
+const TimePickerSheet: React.FC<TimePickerSheetProps> = ({
+  gameTime,
+  visible,
+  dismiss,
+  setGameTime,
+}) => {
+  // set day/month/year not important
+  // date should be + 1 on hours for it to display properly,
+  // this is taken into account when setting the date
+  const [date, setDate] = useState(
+    new Date(1, 1, 1, gameTime.hours, gameTime.minutes, 0),
+  );
+
+  // refresh game time whenever it changes
+  useEffect(() => {
+    setDate(new Date(1, 1, 1, gameTime.hours + 1, gameTime.minutes, 0));
+  }, [gameTime]);
+
+  return (
+    <Sheet dismiss={dismiss} visible={visible} title={'Set move time'}>
+      <View style={styles.container}>
+        <DatePicker
+          style={styles.timeBox}
+          date={date}
+          onDateChange={setDate}
+          mode={'time'}
+          locale={'fr'}
+          is24hourSource={'locale'}
+        />
+        <View style={styles.buttonContainer}>
+          <PrimaryButton
+            style={styles.buttons}
+            label="remove"
+            onPress={() => setGameTime(undefined)}
+          />
+          <PrimaryButton
+            label="confirm"
+            style={styles.buttons}
+            onPress={() => {
+              setGameTime({
+                hours: date.getHours(),
+                minutes: date.getMinutes(),
+              });
+            }}
+          />
+        </View>
+      </View>
+    </Sheet>
+  );
+};
+
+export default TimePickerSheet;
---
+++ packages/TorneloScoresheet/src/components/TimePickerSheet/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingVertical: 15,
+  },
+  timeBox: {
+    marginTop: 25,
+    marginBottom: 65,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  buttons: {
+    height: 70,
+    marginHorizontal: 10,
+  },
+});
