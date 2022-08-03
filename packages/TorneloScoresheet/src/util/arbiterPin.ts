182deb652b524d466a8a630f72bf2c919ad6d76f
gwenllian1 <llio.vernon@gmail.com>
2022-08-03 13:29:32 +1000

    simplified expression

--- packages/TorneloScoresheet/src/util/arbiterPin.ts
+++ packages/TorneloScoresheet/src/util/arbiterPin.ts
@@ -3,5 +3,5 @@
  */
 export const pinValid = (inputPin: string): boolean => {
   const arbiterPin = '1234'; //TODO: change to global variable set by user.
-  return inputPin == arbiterPin ? true : false;
+  return inputPin === arbiterPin;
 };
