9918e2a20b100a68d8c7a14fa7e898db8c2a2b02
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2022-08-04 14:50:33 +1000

    Added the functionality to go back from the arbiterResultDisplay page

--- packages/TorneloScoresheet/src/hooks/appMode/arbiterResultDisplayState.ts
+++ packages/TorneloScoresheet/src/hooks/appMode/arbiterResultDisplayState.ts
@@ -1,14 +1,19 @@
 import { useContext } from 'react';
+import { chessEngine } from '../../chessEngine/chessEngineInterface';
 import {
   AppMode,
   AppModeState,
   ArbiterResultDisplayMode,
 } from '../../types/AppModeState';
+import { fail, Result, succ } from '../../types/Result';
+import { getStoredRecordingModeData } from '../../util/storage';
 
 type arbiterResultDisplayStateHookType = [
   ArbiterResultDisplayMode,
   {
     goToResultDisplayMode: () => void;
+    goBackToEnterPgn: () => void;
+    goBackToRecordingMode: () => Promise<Result<string>>;
   },
 ];
 
@@ -31,5 +36,39 @@ export const makeUseArbiterResultDisplayState =
       });
     };
 
-    return [appModeState, { goToResultDisplayMode }];
+    const goBackToEnterPgn = (): void => {
+      setAppModeState({
+        mode: AppMode.EnterPgn,
+      });
+    };
+
+    const goBackToRecordingMode = async (): Promise<Result<string>> => {
+      const data = await getStoredRecordingModeData();
+      if (data) {
+        const [moveHistory, currentPlayer] = data;
+
+        // will default to starting fen if last element in move hisotry not found
+        // this should never happen
+        const lastFen =
+          moveHistory[moveHistory.length - 1]?.startingFen ??
+          chessEngine.startingFen();
+        setAppModeState({
+          mode: AppMode.GraphicalRecording,
+          currentPlayer,
+          pairing: appModeState.pairing,
+          board: chessEngine.fenToBoardPositions(lastFen),
+          moveHistory,
+        });
+        return succ('');
+      } else {
+        return fail(
+          'Error loading recording mode data, please go back to the PGN screen instead',
+        );
+      }
+    };
+
+    return [
+      appModeState,
+      { goToResultDisplayMode, goBackToEnterPgn, goBackToRecordingMode },
+    ];
   };
