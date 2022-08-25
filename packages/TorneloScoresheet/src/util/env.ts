f2ee5c7d2487f6d41ad9b64b2115294251c7c280
Malo Hamon <hamon.malo@gmail.com>
2022-08-25 17:32:16 +1000

    Add Max # Games stored as env variable
    We now store the maximum number of games to store as an environment var
    It is set to 15 games

--- packages/TorneloScoresheet/src/util/env.ts
+++ packages/TorneloScoresheet/src/util/env.ts
@@ -1,3 +1,4 @@
-import { TORNELO_URL } from '@env';
+import { TORNELO_URL, MAX_PAST_GAME_STORED } from '@env';
 
 export const torneloUrl = TORNELO_URL;
+export const maxPastGameStored = MAX_PAST_GAME_STORED;
