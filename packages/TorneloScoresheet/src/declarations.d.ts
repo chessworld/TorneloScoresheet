275f23266562191f204e5452d925130084c2ce5c
gestureleft <harry130899@hotmail.com>
2022-08-30 18:32:22 +1000

    Added 'react-native-markdown-package' to declarations file
    Since the 'react-native-markdown-package' package doesn't have any type
    declarations, we've added it to our own type declarations file

--- packages/TorneloScoresheet/src/declarations.d.ts
+++ packages/TorneloScoresheet/src/declarations.d.ts
@@ -8,3 +8,8 @@ declare module '*.svg' {
 declare module 'country-iso-3-to-2' {
   export default function (iso3: string): string | undefined;
 }
+
+declare module 'react-native-markdown-package' {
+  const component: React.FC<{ styles: StyleProp<View> }>;
+  export default component;
+}
