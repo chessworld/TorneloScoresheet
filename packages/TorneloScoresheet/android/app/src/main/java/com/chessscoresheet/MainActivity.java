ee1d5d2014652137c29b0acf4b663ea70a9a8ac5
gestureleft <harry130899@hotmail.com>
2022-08-31 14:39:52 +1000

    Update android config to use splash screen

--- packages/TorneloScoresheet/android/app/src/main/java/com/chessscoresheet/MainActivity.java
+++ packages/TorneloScoresheet/android/app/src/main/java/com/chessscoresheet/MainActivity.java
@@ -1,14 +1,15 @@
 package com.torneloscoresheet;
 
-import com.facebook.react.ReactActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
@@ -16,25 +17,26 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        hideNavigationBar();
-    }
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+    hideNavigationBar();
+  }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideNavigationBar();
-        }
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      hideNavigationBar();
     }
+  }
 
-    private void hideNavigationBar() {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-               | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-               | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-               | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-               | View.SYSTEM_UI_FLAG_FULLSCREEN
-               | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
+  private void hideNavigationBar() {
+    getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN |
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+  }
 }
