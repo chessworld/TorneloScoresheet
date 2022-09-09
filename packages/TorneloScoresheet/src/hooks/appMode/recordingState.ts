a44edb1c0370a65a05230eb3dd258e90633bf7c6
Katerina Sova <ksov0001@student.monash.edu>
2022-09-09 16:00:52 +1000

    Implemented 5 fold repetition
    Kat + Llio :)
    All moves and skips are disabled after 5 fold repetition occurs. Undo 'decreases' count of fens so moves can be re-enabled after undoing one of the moves contributing to the 5-fold repetition.

--- packages/TorneloScoresheet/src/hooks/appMode/recordingState.ts
+++ packages/TorneloScoresheet/src/hooks/appMode/recordingState.ts
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { chessEngine } from '../../chessEngine/chessEngineInterface';
 import { MoveReturnType } from '../../chessEngine/chessTsChessEngine';
+import { useError } from '../../context/ErrorContext';
 import {
   AppMode,
   AppModeState,
@@ -68,68 +69,6 @@ const getCurrentFen = (moveHistory: ChessPly[]): string => {
   );
 };
 
-/**
- * Skips a player's turn
- * Will Return the next move history array
- * @param moveHistory ChessPly array of past moves
- * @returns new moveHistory array
- */
-const skipPlayerTurn = (moveHistory: ChessPly[]): ChessPly[] => {
-  const nextPly: SkipPly = {
-    startingFen: getCurrentFen(moveHistory),
-    type: PlyTypes.SkipPly,
-    moveNo: Math.floor(moveHistory.length / 2) + 1,
-    player:
-      moveHistory.length % 2 === 0 ? PlayerColour.White : PlayerColour.Black,
-    drawOffer: false,
-  };
-  return [...moveHistory, nextPly];
-};
-
-/**
- * Processes a player's move given to and from positons
- * Will Return the next move history array
- * Will return null if move is impossible
- * @param moveSquares from and to positions
- * @param moveHistory ChessPly array of past moves
- * @returns new moveHistory array or null
- */
-const processPlayerMove = (
-  moveSquares: MoveSquares,
-  moveHistory: ChessPly[],
-  promotion?: PieceType,
-): ChessPly[] | null => {
-  const startingFen = getCurrentFen(moveHistory);
-
-  // process move
-  const moveSAN = chessEngine.makeMove(
-    startingFen,
-    moveSquares,
-    promotion,
-    MoveReturnType.MOVE_SAN,
-  );
-
-  // return null if move is impossible
-  if (!moveSAN) {
-    return null;
-  }
-
-  // build next play and return new history
-  const nextPly: MovePly = {
-    startingFen,
-    move: moveSquares,
-    type: PlyTypes.MovePly,
-    moveNo: Math.floor(moveHistory.length / 2) + 1,
-    player:
-      moveHistory.length % 2 === 0 ? PlayerColour.White : PlayerColour.Black,
-    promotion,
-    drawOffer: false,
-    san: moveSAN,
-  };
-
-  return [...moveHistory, nextPly];
-};
-
 export const makeUseRecordingState =
   (
     context: React.Context<
@@ -137,11 +76,88 @@ export const makeUseRecordingState =
     >,
   ): (() => recordingStateHookType | null) =>
   (): recordingStateHookType | null => {
+    const [, showError] = useError();
     const [appModeState, setAppModeState] = useContext(context);
 
     if (appModeState.mode !== AppMode.Recording) {
       return null;
     }
+
+    /**
+     * Processes a player's move given to and from positons
+     * Will Return the next move history array
+     * Will return null if move is impossible
+     * @param moveSquares from and to positions
+     * @param moveHistory ChessPly array of past moves
+     * @returns new moveHistory array or null
+     */
+    const processPlayerMove = (
+      moveSquares: MoveSquares,
+      moveHistory: ChessPly[],
+      promotion?: PieceType,
+    ): ChessPly[] | null => {
+      const startingFen = getCurrentFen(moveHistory);
+
+      // process move
+      const moveSAN = chessEngine.makeMove(
+        startingFen,
+        moveSquares,
+        promotion,
+        MoveReturnType.MOVE_SAN,
+      );
+
+      // return null if move is impossible
+      if (!moveSAN) {
+        return null;
+      }
+
+      if (gameInFiveFoldRepetition()) {
+        showError('Illegal move');
+        return null;
+      }
+
+      // build next play and return new history
+      const nextPly: MovePly = {
+        startingFen,
+        move: moveSquares,
+        type: PlyTypes.MovePly,
+        moveNo: Math.floor(moveHistory.length / 2) + 1,
+        player:
+          moveHistory.length % 2 === 0
+            ? PlayerColour.White
+            : PlayerColour.Black,
+        promotion,
+        drawOffer: false,
+        san: moveSAN,
+      };
+
+      return [...moveHistory, nextPly];
+    };
+
+    /**
+     * Skips a player's turn
+     * Will Return the next move history array
+     * @param moveHistory ChessPly array of past moves
+     * @returns new moveHistory array
+     */
+    const skipPlayerTurn = (moveHistory: ChessPly[]): ChessPly[] | null => {
+      if (gameInFiveFoldRepetition()) {
+        showError('Illegal move');
+        return null;
+      }
+      const nextPly: SkipPly = {
+        startingFen: getCurrentFen(moveHistory),
+        type: PlyTypes.SkipPly,
+        moveNo: Math.floor(moveHistory.length / 2) + 1,
+        player:
+          moveHistory.length % 2 === 0
+            ? PlayerColour.White
+            : PlayerColour.Black,
+        drawOffer: false,
+      };
+      return [...moveHistory, nextPly];
+    };
+
     const updateBoard = (moveHistory: ChessPly[]): void => {
       const board = chessEngine.fenToBoardPositions(getCurrentFen(moveHistory));
       setAppModeState({
@@ -188,6 +204,36 @@ export const makeUseRecordingState =
       });
     };
 
+    const inThreeFoldRepetition = (fen: string): boolean => {
+      if (appModeState.pairing.positionOccurances[fen]) {
+        if ((appModeState.pairing.positionOccurances[fen] || 0) > 2) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    const moveInFiveFoldRepetition = (fen: string): boolean => {
+      if (appModeState.pairing.positionOccurances[fen]) {
+        if ((appModeState.pairing.positionOccurances[fen] || 0) > 4) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    const gameInFiveFoldRepetition = (): boolean => {
+      let gameInRepetition = false;
+      if (appModeState.pairing.positionOccurances) {
+        for (let key in appModeState.pairing.positionOccurances) {
+          if (moveInFiveFoldRepetition(key)) {
+            gameInRepetition = true;
+          }
+        }
+      }
+      return gameInRepetition;
+    };
+
     const goToTextInput = (): void => {};
 
     const goToArbiterGameMode = (): void => {
@@ -197,6 +243,35 @@ export const makeUseRecordingState =
       });
     };
 
+    const incrementPositionOccurance = (
+      moveHistory: ChessPly[],
+      moveIndex: number,
+    ): void => {
+      //increase count of fen in moveHistory list
+
+      const key = moveHistory[moveIndex]!.startingFen.split(' ')
+        .slice(0, 4)
+        .join(' ');
+      if (!appModeState.pairing.positionOccurances) {
+        appModeState.pairing.positionOccurances = {};
+      }
+
+      appModeState.pairing.positionOccurances[key] =
+        key in appModeState.pairing.positionOccurances
+          ? (appModeState.pairing.positionOccurances[key] || 0) + 1
+          : 1;
+    };
+
+    const decrementPositionOccurance = (moveIndex: number): void => {
+      const key = appModeState.moveHistory[moveIndex]!.startingFen.split(' ')
+        .slice(0, 4)
+        .join(' ');
+      appModeState.pairing.positionOccurances[key] =
+        key in appModeState.pairing.positionOccurances
+          ? (appModeState.pairing.positionOccurances[key] || 0) - 1
+          : 0;
+    };
+
     const move = (moveSquares: MoveSquares, promotion?: PieceType): void => {
       const moveHistory = processPlayerMove(
         moveSquares,
@@ -204,22 +279,8 @@ export const makeUseRecordingState =
         promotion,
       );
       if (moveHistory !== null) {
+        incrementPositionOccurance(moveHistory, moveHistory.length - 1);
         updateBoard(moveHistory);
-
-        const key = moveHistory[moveHistory.length - 1]!.startingFen.split(' ')
-          .slice(0, 4)
-          .join(' ');
-
-        if (!appModeState.pairing.positionOccurances) {
-          appModeState.pairing.positionOccurances = {};
-        }
-
-        appModeState.pairing.positionOccurances[key] =
-          key in appModeState.pairing.positionOccurances
-            ? (appModeState.pairing.positionOccurances[key] || 0) + 1
-            : 1;
-
-        console.log(appModeState.pairing.positionOccurances);
       }
     };
     const isPawnPromotion = (moveSquares: MoveSquares): boolean => {
@@ -229,15 +290,7 @@ export const makeUseRecordingState =
 
     const undoLastMove = (): void => {
       if (appModeState.moveHistory.length > 0) {
-        const key = appModeState.moveHistory[
-          appModeState.moveHistory.length - 1
-        ]!.startingFen.split(' ')
-          .slice(0, 4)
-          .join(' ');
-        appModeState.pairing.positionOccurances[key] =
-          key in appModeState.pairing.positionOccurances
-            ? (appModeState.pairing.positionOccurances[key] || 0) - 1
-            : 0;
+        decrementPositionOccurance(appModeState.moveHistory.length - 1);
       } else {
         //game reset to start - clear position memory
         appModeState.pairing.positionOccurances = {};
@@ -246,7 +299,10 @@ export const makeUseRecordingState =
     };
 
     const skipTurn = (): void => {
-      updateBoard(skipPlayerTurn(appModeState.moveHistory));
+      let chessMove = skipPlayerTurn(appModeState.moveHistory);
+      if (chessMove) {
+        updateBoard(chessMove);
+      }
     };
 
     const isOtherPlayersPiece = (moveSquares: MoveSquares): boolean => {
@@ -260,12 +316,16 @@ export const makeUseRecordingState =
       moveSquares: MoveSquares,
       promotion?: PieceType,
     ): void => {
-      const historyAfterSkip = skipPlayerTurn(appModeState.moveHistory);
+      let historyAfterSkip = skipPlayerTurn(appModeState.moveHistory);
+      if (!historyAfterSkip) {
+        return;
+      }
       const historyAfterSkipAndMove = processPlayerMove(
         moveSquares,
         historyAfterSkip,
         promotion,
       );
+      incrementPositionOccurance(historyAfterSkip, historyAfterSkip.length - 1);
       if (historyAfterSkipAndMove !== null) {
         updateBoard(historyAfterSkipAndMove);
       } else {
