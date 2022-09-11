182b55db61259c25be5bc5a1dbbf66ddab1fc39a
Malo Hamon <hamon.malo@gmail.com>
2022-09-11 13:52:12 +1000

    Move `plyToMoves` function to utils
    This is the function that converts from a list of plys to a list of moves
    
    It now resides in a util file since it is shared by many components

---
+++ packages/TorneloScoresheet/src/util/moves.ts
@@ -0,0 +1,24 @@
+import { PlayerColour } from '../types/ChessGameInfo';
+import { ChessPly, Move, PlyTypes } from '../types/ChessMove';
+
+export const moveString = (ply: ChessPly, isEditing: boolean): string => {
+  if (isEditing) {
+    return '____';
+  }
+  if (ply.type === PlyTypes.SkipPly) {
+    return '-';
+  }
+
+  return ply.san;
+};
+
+// Utility function to take a list of ply, and return a list of moves
+export const plysToMoves = (ply: ChessPly[]): Move[] =>
+  ply.reduce((acc, el) => {
+    if (el.player === PlayerColour.White) {
+      return [...acc, { white: el, black: undefined }];
+    }
+    return acc
+      .slice(0, -1)
+      .concat({ white: acc[acc.length - 1]!.white, black: el });
+  }, [] as Move[]);
