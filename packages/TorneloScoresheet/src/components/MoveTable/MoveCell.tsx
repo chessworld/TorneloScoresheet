094a4266980a3a1c7a0e13be4a55848417921ce6
Malo Hamon <hamon.malo@gmail.com>
2022-09-11 13:52:44 +1000

    Added `MoveTable` component
    This is a component that display the move history in a table format
    
    It takes in a list of `ChessPly`

---
+++ packages/TorneloScoresheet/src/components/MoveTable/MoveCell.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View } from 'react-native';
+import { colours, ColourType } from '../../style/colour';
+import { ChessPly, GameTime } from '../../types/ChessMove';
+import { moveString } from '../../util/moves';
+import PrimaryText from '../PrimaryText/PrimaryText';
+import { styles } from './style';
+import Icon from 'react-native-vector-icons/Entypo';
+
+export type MoveCellProps = {
+  ply?: ChessPly;
+  highlightColor?: ColourType;
+};
+
+const MoveCell: React.FC<MoveCellProps> = ({ ply, highlightColor }) => {
+  const formatTime = (time?: GameTime): string => {
+    if (!time) {
+      return '--:--';
+    }
+
+    const padWith0 = (value: number): string => {
+      return value > 9 ? value.toString() : `0${value}`;
+    };
+    return `${padWith0(time.hours)}:${padWith0(time.minutes)}`;
+  };
+  return (
+    <View style={styles.moveCellContainer}>
+      <View
+        style={[styles.moveSanTextBox, { backgroundColor: highlightColor }]}>
+        {ply && <PrimaryText size={25} label={moveString(ply, false)} />}
+      </View>
+
+      <View style={styles.moveTimeTextBox}>
+        {ply?.drawOffer && (
+          <View style={styles.drawOfferIcon}>
+            <Icon
+              name="creative-commons-noderivs"
+              size={16}
+              color={colours.darkGrey}
+            />
+          </View>
+        )}
+        <PrimaryText label={formatTime(ply?.gameTime)} size={20} />
+      </View>
+    </View>
+  );
+};
+
+export default MoveCell;
