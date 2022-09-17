eced40cd2929120ca2d45630dd801662f4b279c3
gestureleft <harry130899@hotmail.com>
2022-09-17 16:22:17 +1000

    Icon button: Added ability to have a label

---
+++ packages/TorneloScoresheet/src/components/IconButton/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  label: {
+    marginLeft: 4,
+  },
+});
