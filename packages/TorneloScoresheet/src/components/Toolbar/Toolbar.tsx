add0d7c58605c740ea35cd44a37b13df84981187
gestureleft <harry130899@hotmail.com>
2022-09-17 15:22:46 +1000

    Moved the "go to history" button to the toolbar
    It is now a little history icon

--- packages/TorneloScoresheet/src/components/Toolbar/Toolbar.tsx
+++ packages/TorneloScoresheet/src/components/Toolbar/Toolbar.tsx
@@ -13,7 +13,10 @@ import PrimaryText, { FontWeight } from '../PrimaryText/PrimaryText';
 import Sheet from '../Sheet/Sheet';
 import { styles } from './style';
 import ToggleRecordingMode from './ToggleRecordingMode';
-import { useAppModeState } from '../../context/AppModeStateContext';
+import {
+  useAppModeState,
+  useEnterPgnState,
+} from '../../context/AppModeStateContext';
 import ArbiterAndPlayerModeDisplay from './ArbiterAndPlayerModeDisplay';
 import packageJson from '../../../package.json';
 import Help from '../Help/Help';
@@ -33,6 +36,7 @@ const backgroundColorStyle = (backgroundColor: string) => ({
 
 const Toolbar: React.FC = () => {
   const appModeState = useAppModeState();
+  const enterPgnMode = useEnterPgnState();
   const [showSheet, setShowSheet] = useState(false);
   const currentColour = colourForMode(appModeState.mode);
   const currentTextColour = textColour(currentColour);
@@ -52,6 +56,15 @@ const Toolbar: React.FC = () => {
       </Sheet>
       <View style={[styles.container, backgroundColorStyle(currentColour)]}>
         <View style={styles.arbiterLock}>
+          {appModeState.mode === AppMode.EnterPgn && (
+            <IconButton
+              icon="history"
+              onPress={() => {
+                enterPgnMode?.viewPastGames();
+              }}
+              colour="black"
+            />
+          )}
           <ArbiterAndPlayerModeDisplay currentTextColour={currentTextColour} />
         </View>
         <View style={styles.logo}>
