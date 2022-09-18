d1f5dc51388f5442e92e42aaf88ec08f6f018d67
gwenllian1 <llio.vernon@gmail.com>
2022-09-18 20:10:29 +1000

    added popup sheet to inform user illegal moves will be skipped kat+llio

--- packages/TorneloScoresheet/src/pages/GraphicalRecording/MoveOptionsSheet.tsx
+++ packages/TorneloScoresheet/src/pages/GraphicalRecording/MoveOptionsSheet.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sheet from '../../components/Sheet/Sheet';
 import { colours } from '../../style/colour';
 import { ICON_CLOCK, ICON_HALF } from '../../style/images';
@@ -7,6 +7,7 @@ import MoveOption from './MoveOption';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { useRecordingState } from '../../context/AppModeStateContext';
+import OptionSheet from '../../components/OptionSheet/OptionSheet';
 
 export type EditingMove = {
   colour: PlayerColour;
@@ -25,6 +26,7 @@ const MoveOptionsSheet = ({
   dismiss,
 }: MoveOptionsSheetProps) => {
   const recordingState = useRecordingState();
+  const [displaySkipMoves, setDisplaySkipMoves] = useState(false);
   const actions = recordingState?.[1];
 
   const toggleDraw = actions?.toggleDraw;
@@ -39,52 +41,68 @@ const MoveOptionsSheet = ({
     );
     dismiss();
   };
+  const handleConfirmSkips = () => {
+    setDisplaySkipMoves(false);
+    if (editingMove) {
+      actions?.goToEditMove(
+        editingMove.moveIndex * 2 +
+          (editingMove.colour === PlayerColour.Black ? 1 : 0),
+      );
+    }
+    dismiss();
+  };
 
   return (
-    <Sheet
-      visible={Boolean(editingMove)}
-      dismiss={dismiss}
-      title="Move options">
-      <MoveOption
-        onPress={() => {
-          if (editingMove) {
-            handleGameTime(
-              editingMove.moveIndex * 2 +
-                (editingMove.colour === PlayerColour.Black ? 1 : 0),
-            );
-          }
-          dismiss();
-        }}
-        colour={colours.primary}
-        icon={<ICON_CLOCK color={colours.white} />}
-        label="Record time"
+    <>
+      <OptionSheet
+        visible={displaySkipMoves}
+        message="Illegal Moves Will Be Replaced with Skips"
+        onCancel={() => setDisplaySkipMoves(false)}
+        options={[
+          {
+            text: 'Confirm',
+            onPress: handleConfirmSkips,
+          },
+        ]}
       />
-      <MoveOption
-        onPress={handleDrawOffer}
-        colour={colours.primary}
-        icon={<ICON_HALF color={colours.white} />}
-        label="Draw offer"
-      />
-      <MoveOption
-        onPress={() => {
-          if (editingMove) {
-            actions?.goToEditMove(
-              editingMove.moveIndex * 2 +
-                (editingMove.colour === PlayerColour.Black ? 1 : 0),
-            );
-          }
-        }}
-        colour={colours.primary}
-        icon={<Icon name="edit" size={35} color={colours.white} />}
-        label="Edit move"
-      />
-      <MoveOption
-        onPress={dismiss}
-        colour={colours.secondary}
-        icon={<MaterialIcon name="cancel" size={35} color={colours.white} />}
-        label="Cancel"
-      />
-    </Sheet>
+      <Sheet
+        visible={Boolean(editingMove)}
+        dismiss={dismiss}
+        title="Move options">
+        <MoveOption
+          onPress={() => {
+            if (editingMove) {
+              handleGameTime(
+                editingMove.moveIndex * 2 +
+                  (editingMove.colour === PlayerColour.Black ? 1 : 0),
+              );
+            }
+            dismiss();
+          }}
+          colour={colours.primary}
+          icon={<ICON_CLOCK color={colours.white} />}
+          label="Record time"
+        />
+        <MoveOption
+          onPress={handleDrawOffer}
+          colour={colours.primary}
+          icon={<ICON_HALF color={colours.white} />}
+          label="Draw offer"
+        />
+        <MoveOption
+          onPress={() => setDisplaySkipMoves(true)}
+          colour={colours.primary}
+          icon={<Icon name="edit" size={35} color={colours.white} />}
+          label="Edit move"
+        />
+        <MoveOption
+          onPress={dismiss}
+          colour={colours.secondary}
+          icon={<MaterialIcon name="cancel" size={35} color={colours.white} />}
+          label="Cancel"
+        />
+      </Sheet>
+    </>
   );
 };
 
