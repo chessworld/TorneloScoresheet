5f22898f6ed9a7fdb37e9d71a58af35cad9da9d1
Malo Hamon <hamon.malo@gmail.com>
2022-10-12 13:39:56 +1100

    add email endpoint to env settings

--- packages/TorneloScoresheet/src/util/env.ts
+++ packages/TorneloScoresheet/src/util/env.ts
@@ -1,4 +1,7 @@
-import { TORNELO_URL, MAX_PAST_GAME_STORED } from '@env';
+import { TORNELO_URL, MAX_PAST_GAME_STORED, EMAIL_API_ENDPOINT } from '@env';
 
 export const torneloUrl = TORNELO_URL;
 export const maxPastGameStored = MAX_PAST_GAME_STORED;
+export const emailEndpoint = (arbiterId: string): string => {
+  return EMAIL_API_ENDPOINT.replace('USER_ID', arbiterId);
+};
