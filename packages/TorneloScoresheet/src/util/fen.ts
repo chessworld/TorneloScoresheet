299c7be51e6a539ae2148ce507325df9c42e1333
Malo Hamon <hamon.malo@gmail.com>
2022-10-15 15:32:04 +1100

    Add fen util function
    This function will extract the state information from the fen and discard the rest

---
+++ packages/TorneloScoresheet/src/util/fen.ts
@@ -0,0 +1,19 @@
+/**
+ * Returns the board state section of a fen string
+ * @param fen the current fen
+ * @returns the state part of the fen or an empty string if the fen is invalid
+ */
+export const getStateFromFen = (fen: string): string => {
+  const regexResult =
+    /(?<boardState>.+\/.+\/.+\/.+\/.+\/.+\/.+\/.+?\s.{1})(?<game_state>\s.+?\s.+?\s.+?\s.+?)?/.exec(
+      fen,
+    );
+  if (
+    !regexResult ||
+    !regexResult.groups ||
+    !regexResult.groups['boardState']
+  ) {
+    return '';
+  }
+  return regexResult.groups['boardState'];
+};
