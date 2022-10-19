4c7554dc15ca3e1d714f6c435be64ae94f3988b2
Malo Hamon <hamon.malo@gmail.com>
2022-10-19 13:15:12 +1100

    Make Piece asset accept style argument
    This is optional and will default to tornelo style

--- packages/TorneloScoresheet/src/components/PieceAsset/PieceAsset.tsx
+++ packages/TorneloScoresheet/src/components/PieceAsset/PieceAsset.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
-import { Color } from 'react-native-svg';
+import { Color, SvgProps } from 'react-native-svg';
 import { colours } from '../../style/colour';
-import { BISHOP, KNIGHT, QUEEN, ROOK, KING, PAWN } from '../../style/images';
+import {
+  TORNELO_BISHOP,
+  TORNELO_KNIGHT,
+  TORNELO_QUEEN,
+  TORNELO_ROOK,
+  TORNELO_KING,
+  TORNELO_PAWN,
+  CLASSIC_BISHOP,
+  CLASSIC_KNIGHT,
+  CLASSIC_QUEEN,
+  CLASSIC_ROOK,
+  CLASSIC_KING,
+  CLASSIC_PAWN,
+} from '../../style/images';
 import { PlayerColour } from '../../types/ChessGameInfo';
 import { PieceType, Piece } from '../../types/ChessMove';
+import { ChessPieceStyles } from '../../types/GeneralSettingsState';
 
 export type PieceAssetProps = {
   piece: Piece;
+  pieceStyle?: ChessPieceStyles;
   size: number;
   colour?: Color | undefined;
   style?: StyleProp<ViewStyle>;
@@ -15,80 +30,52 @@ export type PieceAssetProps = {
 
 const PieceAsset: React.FC<PieceAssetProps> = ({
   piece,
+  pieceStyle,
   size,
   colour,
   style,
 }) => {
+  const selectedStyle =
+    pieceStyle === undefined ? ChessPieceStyles.TORNELO : pieceStyle;
+  const torneloPieceRecord: Record<PieceType, React.FC<SvgProps>> = {
+    [PieceType.Bishop]: TORNELO_BISHOP,
+    [PieceType.Pawn]: TORNELO_PAWN,
+    [PieceType.Rook]: TORNELO_ROOK,
+    [PieceType.Queen]: TORNELO_QUEEN,
+    [PieceType.King]: TORNELO_KING,
+    [PieceType.Knight]: TORNELO_KNIGHT,
+  };
+
+  const classicPieceRecord: Record<PieceType, React.FC<SvgProps>> = {
+    [PieceType.Bishop]: CLASSIC_BISHOP,
+    [PieceType.Pawn]: CLASSIC_PAWN,
+    [PieceType.Rook]: CLASSIC_ROOK,
+    [PieceType.Queen]: CLASSIC_QUEEN,
+    [PieceType.King]: CLASSIC_KING,
+    [PieceType.Knight]: CLASSIC_KNIGHT,
+  };
+
+  const pieceRecord: Record<
+    ChessPieceStyles,
+    Record<PieceType, React.FC<SvgProps>>
+  > = {
+    [ChessPieceStyles.CLASSIC]: classicPieceRecord,
+    [ChessPieceStyles.TORNELO]: torneloPieceRecord,
+  };
   const fill =
     colour ??
     (piece.player === PlayerColour.Black ? colours.black : colours.white);
   const pieceStroke =
     colour ??
     (piece.player === PlayerColour.White ? colours.black : colours.white);
-  switch (piece.type) {
-    case PieceType.Bishop:
-      return (
-        <BISHOP
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    case PieceType.Rook:
-      return (
-        <ROOK
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    case PieceType.King:
-      return (
-        <KING
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    case PieceType.Queen:
-      return (
-        <QUEEN
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    case PieceType.Knight:
-      return (
-        <KNIGHT
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    case PieceType.Pawn:
-      return (
-        <PAWN
-          style={style}
-          height={size}
-          width={size}
-          fill={fill}
-          stroke={pieceStroke}
-        />
-      );
-    default:
-      return <>Unknown Piece</>;
-  }
+
+  return pieceRecord[selectedStyle][piece.type]({
+    style,
+    height: size,
+    width: size,
+    fill,
+    stroke: pieceStroke,
+  });
 };
 
 export default PieceAsset;
