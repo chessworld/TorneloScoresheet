41b073cbe3af38c9ef56c2860169200356aa2254
Malo Hamon <hamon.malo@gmail.com>
2022-11-07 11:58:18 +1100

    Update env variables to use production api

--- packages/TorneloScoresheet/src/env.d.ts
+++ packages/TorneloScoresheet/src/env.d.ts
@@ -1,5 +1,5 @@
 declare module '@env' {
-  export const TORNELO_URL: string;
-  export const EMAIL_API_ENDPOINT: string;
-  export const MAX_PAST_GAME_STORED: number;
+  export const REACT_APP_TORNELO_URL: string;
+  export const REACT_APP_EMAIL_API_ENDPOINT: string;
+  export const REACT_APP_MAX_PAST_GAME_STORED: number;
 }
--- packages/TorneloScoresheet/src/util/env.ts
+++ packages/TorneloScoresheet/src/util/env.ts
@@ -1,7 +1,11 @@
-import { TORNELO_URL, MAX_PAST_GAME_STORED, EMAIL_API_ENDPOINT } from '@env';
+import {
+  REACT_APP_TORNELO_URL,
+  REACT_APP_MAX_PAST_GAME_STORED,
+  REACT_APP_EMAIL_API_ENDPOINT,
+} from '@env';
 
-export const torneloUrl = TORNELO_URL;
-export const maxPastGameStored = MAX_PAST_GAME_STORED;
+export const torneloUrl = REACT_APP_TORNELO_URL;
+export const maxPastGameStored = REACT_APP_MAX_PAST_GAME_STORED;
 export const emailEndpoint = (arbiterId: string): string => {
-  return EMAIL_API_ENDPOINT.replace('USER_ID', arbiterId);
+  return REACT_APP_EMAIL_API_ENDPOINT.replace('USER_ID', arbiterId);
 };
