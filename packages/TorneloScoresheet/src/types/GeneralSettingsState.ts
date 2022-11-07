fa9c76bf37ad19156b492169473a7bd937a2414b
Malo Hamon <hamon.malo@gmail.com>
2022-11-07 12:33:26 +1100

    Add Board Style in general Settings

--- packages/TorneloScoresheet/src/types/GeneralSettingsState.ts
+++ packages/TorneloScoresheet/src/types/GeneralSettingsState.ts
@@ -1,12 +1,19 @@
 export enum ChessPieceStyles {
-  TORNELO,
-  CLASSIC,
+  TORNELO = 'TORNELO',
+  CLASSIC = 'CLASSIC',
+}
+
+export enum ChessBoardStyles {
+  TORNELO = 'TORNELO',
+  CLASSIC = 'CLASSIC',
 }
 
 export type GeneralSettings = {
   chessPieceStyle: ChessPieceStyles;
+  chessBoardStyle: ChessBoardStyles;
 };
 
 export const defaulGeneralSettings: GeneralSettings = {
   chessPieceStyle: ChessPieceStyles.CLASSIC,
+  chessBoardStyle: ChessBoardStyles.CLASSIC,
 };
